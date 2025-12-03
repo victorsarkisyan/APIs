970e2317e1bb2865ccab3169840167caa800d882
victorsarkisyan <54569499+victorsarkisyan@users.noreply.github.com>
2025-12-02 18:17:34 -0800

    Create GetAllSwitches.py

---
+++ GetAllSwitches.py
@@ -0,0 +1,24 @@
+import requests
+import json
+import sys
+import urllib3
+
+# Disable insecure request warnings
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+ 
+api_key = sys.argv[1]
+controller_ip = sys.argv[2]
+
+url = "https://{controller_ip}/api/v2/monitor/switch-controller/managed-switch/status"
+
+headers = {
+  'Content-Type': 'application/json',
+  'Authorization': f'Bearer {api_key}'   
+}
+
+response = requests.get(url, headers=headers, verify=False)
+
+output = response.json()
+
+switch_ids = [item["switch-id"] for item in output["results"]]
+print(", ".join(switch_ids))
