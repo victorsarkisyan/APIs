854a3b64fd6d8b9140cb1a52a875c6d13bc22f41
victorsarkisyan <54569499+victorsarkisyan@users.noreply.github.com>
2026-04-16 21:59:03 -0700

    Update F_GetAPRadioStats.py
    Added the filter per WAP

--- F_GetAPRadioStats.py
+++ F_GetAPRadioStats.py
@@ -9,15 +9,17 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 api_key = sys.argv[1]
 controller_ip = sys.argv[2]
+ap_id = sys.argv[3]
 
 url = f"https://{controller_ip}/api/v2/monitor/wifi/managed_ap"
+params = {"wtp_id": ap_id}
 
 headers = {
     "Content-Type": "application/json",
     "Authorization": f"Bearer {api_key}"
 }
 
-response = requests.get(url, headers=headers, verify=False)
+response = requests.get(url, headers=headers, params=params, verify=False)
 
 data = response.json()
 results = data.get("results", [])
